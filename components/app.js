1e2e4393b4a4d86cf0a0714676fe4530ac1e7f44
Rebecca Townsend <rebecca.townsend722@gmail.com>
2017-11-10 11:35:49 -0800

    Render app component on page

---
+++ components/app.js
@@ -0,0 +1,12 @@
+angular.module('main')
+
+  .component('app', {
+    // controller: function() {
+    //   //context variables
+    //   //methods
+    // },
+    templateUrl: './templates/app.html'
+  });
+
+
+
